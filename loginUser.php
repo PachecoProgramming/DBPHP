65981bb9f9a562f5fca53570ffe38473d9964ec5
PachecoProgramming <anthonypacheco@my.ccsu.edu>
2016-04-30 11:29:01 -0400

    Update loginUser.php

--- loginUser.php
+++ loginUser.php
@@ -1,27 +1,33 @@
-<?php
-if($_SERVER['REQUEST_METHOD']=='POST'){
-require 'dbconnect.php';
-isUserGood();
-}
+<?php 
 
-function isUserGood()
+include "dbconnect.php";
+
+if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']))
 {
-  global $connect;
-  $username = $_POST["username"];
-  $password = $_POST["password"];
-  $response = array();
-  $response["success"] = false;
-  $sql = 'SELECT username, password FROM Users WHERE username = `username` AND password= `password` ';
-  $result = $sql->query();
-  echo $sql;
-  if($result)
-  {
-    $response["success"] = true;
-    echo $response;
-  }
-  else
-  { 
-    echo "Invalid Login"; 
-  }
+    echo "<h1User already logged in</h1>";
 }
-?>	
+elseif(!empty($_POST['Username']) && !empty($_POST['Password']))
+{
+    $username = mysql_real_escape_string($_POST['Username']);
+    $password = md5(mysql_real_escape_string($_POST['Password']));
+     
+    $checklogin = mysql_query("SELECT * FROM Users WHERE Username = '".$username."' AND Password = '".$password."'");
+     
+    if(mysql_num_rows($checklogin) == 1)
+    {
+        $row = mysql_fetch_array($checklogin);
+        $fname = $row['FirstName'];
+         
+        $_SESSION['Username'] = $username;
+        $_SESSION['Password'] = $password;
+        $_SESSION['LoggedIn'] = "Success";
+         
+        echo "<h1>Success</h1>";
+    }
+    else
+    {
+        echo "<h1>Error</h1>";
+        echo "<p>Sorry, your account could not be found.</p>";
+    }
+}
+?>
