e0d7173c52eda57c9e83485678f17ca3a92c5374
PachecoProgramming <anthonypacheco@my.ccsu.edu>
2016-05-01 12:36:57 -0400

    Update registerUser.php

--- registerUser.php
+++ registerUser.php
@@ -3,47 +3,56 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 require 'dbconnect.php';
 registerUser();
 }
-
+?>
+<?php
 function registerUser()
 {
 	global $connect;
-    if($connect)
-    {
+    	if($connect)
+    	{
 	    $FirstName = $_POST["FirstName"];
 	    $LastName = $_POST["LastName"];
 	    $Username = $_POST["Username"];
 	    $Password = $_POST["Password"];
-        $response = array();
+	    $response = array();
+	    $response["Success"] = false;
 
-        if(!empty($_POST["FirstName"]) && !empty($_POST["LastName"]) && !empty($_POST["Username"]) && !empty($_POST["Password"]))
-        {
-
+        	if(!empty($_POST["FirstName"]) && !empty($_POST["LastName"]) && !empty($_POST["Username"]) && !empty($_POST["Password"]))
+        	{
 		    $sql = "SELECT * FROM Users WHERE username='$username'";
 		    $check = mysqli_fetch_array(mysqli_query($connect,$sql));
 		    if(isset($check))
-                {
-			        echo 'username already exist';
+                	{
+			        echo json_encode('username already exist');
+			        return $response["Success"];
 		        }
-                else
-                {				
+                	else
+                	{				
 		        	$query = " Insert into Users(FirstName,LastName,Username,Password) values ('$FirstName','$LastName','$Username','$Password');";
 			        if(mysqli_query($connect,$sql))
-                        {
-			                	echo 'successfully registered';
-                                $response["success"] = true;
-			            }
-                        else
-                        {
-				                echo 'oops! Please try again!';
-                                $response["success"] = false;
-			            }
+                        	{
+                        		echo json_encode('successfully registered');
+                                	$response["success"] = true;
+                                	return $response["Success"];
+                        	}
+                        	else
+                        	{
+                        		echo json_encode('oops! Please try again!');
+                                	$response["success"] = false;
+                                	return $response["Success"];
+                        		
+                        	}
 		        }
-		mysqli_close($connect);
-	   }
-    }
-}
-else
-{
-    echo 'connection to database failed';
+				mysqli_close($connect);
+	   	}
+	   	else
+	   	{
+	   		echo json_encode('Bad request');
+	   	}
+    	}
+    	else
+    	{
+    		echo json_encode('connection to database failed');
+    	}
 }
 ?>
